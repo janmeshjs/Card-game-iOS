718199dec1a0355c60e6af7b98f12639ec9b31ab
janmesh Singh <Janmesh.js@gmail.com>
2023-02-26 15:46:52 +0530

    Initial Commit

---
+++ War card game/War_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_card_gameApp.swift
+//  War card game
+//
+//  Created by Janmesh Singh on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct War_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
